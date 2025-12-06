ae898bc2994bb1d2ee1093646089b7af27a05e43
Joan Martínez <128309614+joanmrtnz@users.noreply.github.com>
2025-12-06 16:21:11 +0100

    feat(api, core): add api app and core package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,8 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { helloCore } from "@repo/core";
+
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
@@ -33,7 +35,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
+           { helloCore() }
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
---
+++ packages/core/src/index.ts
@@ -0,0 +1,1 @@
+export const helloCore = () => "Core works!";
