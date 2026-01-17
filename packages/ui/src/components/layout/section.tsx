61cb03a19a6892e7c40240dd8694e2cff21754b2
jnmrtnz <joanmartinez.dev@gmail.com>
2026-01-17 15:57:40 +0100

    feat(ui): extend Section component with additional layout props

--- packages/ui/src/components/layout/section.tsx
+++ packages/ui/src/components/layout/section.tsx
@@ -3,12 +3,18 @@ import styles from "./section.module.css";
 export function Section({
   children,
   spacing = "md",
+  width = "full",
+  align = "left",
 }: {
   children: React.ReactNode;
   spacing?: "xs" | "sm" | "md" | "lg" | "xl" | "x2l" | "x3l";
+  width?: "full" | "70" | "50";
+  align?: "center" | "left" | "right";
 }) {
   return (
-    <div className={`${styles.section} ${styles[spacing]}`}>
+    <div
+      className={`${styles.section} ${styles[spacing]} ${styles[`width-${width}`]} ${styles[`align-${align}`]}`}
+    >
       {children}
     </div>
   );
