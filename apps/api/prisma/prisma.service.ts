acdf42a767ba5fd611a4b54f8c0ff05827b4c0c6
jnmrtnz <joanmartinez.dev@gmail.com>
2026-03-04 21:06:43 +0100

    refactor(user): rename passwordHash to password column

--- apps/api/prisma/prisma.service.ts
+++ apps/api/prisma/prisma.service.ts
@@ -1,16 +1,15 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client/extension';
-
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit
-{
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    super({
-      datasourceUrl: process.env.DATABASE_URL,
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
     });
+
+    super({ adapter });
   }
 
   async onModuleInit() {
