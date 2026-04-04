5236c2e2342e02f600c116eda43038f87550eeb4
jnmrtnz <joanmartinez.dev@gmail.com>
2026-04-04 23:42:57 +0200

    feat(api): add profile fields to user prisma schema

---
+++ apps/api/prisma/migrations/20260404211245_add_user_profile_fields/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `dateOfBirth` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `fullName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `mobileNumber` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "dateOfBirth" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "fullName" TEXT NOT NULL,
+ADD COLUMN     "mobileNumber" TEXT NOT NULL;
